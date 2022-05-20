59990646e8c0780e9c38b4c8a15588b2b50e4af1
lark02 <louierabi2002@gmail.com>
2022-05-20 12:17:42 +0800

    tour updated

---
+++ tour3.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Boac Tour</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <img src="images/boachead.png">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="tour.php"class="active">Tours</a></li>
+                    <li><a href="about-us.php">About us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+
+           <div class="places">
+                <div class="middle">
+                    <h2>Place to Visit</h2>
+                 <ul>
+                     <li><a href="tour1.php">Boac Cathedral</a></li>
+                     <li><a href="tour2.php">Casa Real</a></li>
+                     <li><a href="tour3.php">Boac National Museum</a></li>
+                     <li><a href="tour4.php">Biglang Awa Bridge</a></li>
+                     <li><a href="tour5.php">Laylay Port</a></li>
+                     <li><a href="tour6.php">Battle of Paye</a></li>
+                     <li><a href="tour7.php">Trivinios' House</a></li>
+                     <li><a href="tour8.php">Casa Narvas</a></li>
+                 </ul>
+                </div>
+            </div>
+            
+			
+			<div class="Tour1">
+                <div>
+                       <h2>Boac National Museum</h2>
+                       <img src="images/tour3.jpg">
+                       <h4> source: <a href="https://www.nationalmuseum.gov.ph/our-museums/regional-area-and-site-museums/marinduque-romblom/">Boac National Museum Image Link </a> </h4>
+                       <?php
+                        echo "<p>  The branch museum is housed in an old Spanish building that used to be a municipal court and library.  Its collection includes materials reflecting the heritage and history of the region.</br>  On display are agricultural implements, pottery molds, bamboo tuba (coconut wine) containers, modern wood carvings, precolonial tradeware ceramics from China, local geologic materials like minerals, fossilized shells and rocks.  Highlights of the collection are the wooden percussion intrument kalutang  and Moriones festival masks and costumes.  The Moriones festival is a Lenten tradition in Marinduque that centers on Longinus, the one-eyed Roman centurion who got back his full sight when blood of the crucified Jesus spurted into his blind eye.</p>";
+                        ?>
+                </div>
+            </div>
+            
+            
+			
+        </div>
+     </div>
+    
+    </body>
+</html>
