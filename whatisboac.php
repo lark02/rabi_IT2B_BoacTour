a1f93ae81ce24cfb297ce8cdb4d742092c723728
lark02 <louierabi2002@gmail.com>
2022-05-20 11:36:24 +0800

    boac info added

---
+++ whatisboac.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>BOAC TOUR</title>
+		<link rel="stylesheet" href="css/style.css">
+		
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="header">
+				<img src="images/boachead.png">
+			</div>
+		<div class="nav">
+				<ul>
+					<li><a href="home.php"  class="active">Home</a></li>
+					<li><a href="tour.php">Tours</a></li>
+					<li><a href="about-us.php">About us</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+			<div class="banner">
+				<img src="images/banner.png">
+			</div>
+			
+			<div class="main">
+				<div class="left-side-bar">
+					<h2>Menu</h2>
+                 <ul>
+                     <li><a href="whatisboac.php" >What is Boac?</a></li>
+                     <li><a href="whereisboac.php">Where is Boac?</a></li>
+					 <li><a href="history.php">History</a></li>
+                 </ul>
+				</div>
+				
+				<div class="right-side">
+					<img src="images/map.png">
+				</div>
+				
+			
+				<div class="content">
+					<h1>Home</h1>
+					
+					<h2 id="What is Boac?"> What is Boac?</h2>
+					<?php
+					echo "<p>According to the website of Marinduque, Boac, officially the Municipality of Boac (Tagalog: Bayan ng Boac), is a 1st class municipality and capital of the province of Marinduque, Philippines. According to the 2020 census, it has a population of 57,283 people making the Most populous town in Marinduque.</p>";
+					echo "<p>Additionally,it is a heritage town in its own right, is known as one of the main venues of the annual Moriones Festival and is the location of a number of historical sites in Marinduque including the Boac Cathedral fortress church, Casa Real and the Liwasan ng Kalayaan, Laylay Port, and the Battle of Paye site. The municipality is also home to the Marinduque Branch of the National Museum of the Philippines.</p>";
+					echo "<p>Moreover, Boac is the third largest of the six municipalities of the province of Marinduque, after Santa Cruz and Torrijos. It borders all the municipalities of the province: Mogpog to the north, Santa Cruz to the Northeast, Torrijos to the East, Buenavista to the South and Gasan to the South Southwest. The municipality is generally hilly, rugged and mountainous in the south and eastern part with thin strips of flat and farm lands and long shoreline in the west. The longest and largest river, Boac River, which gets its source in the mountainous forest in the extreme southwest and spills off to the northern shore of Boac, divides the municipality into two geographical areas: north and south.</p>";
+					?>
+				</div>
+				
+				<div class="footer">
+					<p>&copy; 2022 Marinduque State College<br> Lark Louie M. Rabi, Lupac,Boac, Marinduque- Philippines<br>.All rights reserved.</p>
+					
+					<a href="https://www.facebook.com/louie.rabi"><img src="images/fb.jpg"></a>
+					<a href="https://www.instagram.com/larkkkyyy/"><img src="images/ig.jpg"></a>
+				</div>
+		
+		</div>
+	</body>
+</html>
