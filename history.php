b436e7fbb5701e9f0c01879ad950d65c0b82a0b5
lark02 <louierabi2002@gmail.com>
2022-05-20 11:55:41 +0800

    history info added

---
+++ history.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>BOAC TOUR</title>
+		<link rel="stylesheet" href="css/style.css">
+		
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="header">
+				<img src="images/boachead.png">
+			</div>
+		<div class="nav">
+				<ul>
+					<li><a href="home.php"  class="active">Home</a></li>
+					<li><a href="tour.php">Tours</a></li>
+					<li><a href="about-us.php">About us</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+			<div class="banner">
+				<img src="images/banner.png">
+			</div>
+			
+			<div class="main">
+				<div class="left-side-bar">
+					<h2>Menu</h2>
+                 <ul>
+ 					 <li><a href="whatisboac.php">What is Boac?</a></li>
+                     <li><a href="whereisboac.php">Where is Boac?</a></li>
+					 <li><a href="history.php" >History</a></li>
+                 </ul>
+				</div>
+				
+				<div class="right-side">
+					<img src="images/map.png">
+				</div>
+				
+			
+				<div class="content">
+					<h1>Home</h1>
+					
+					<h2 id="History">History</h2>
+
+
+					<?php
+
+					echo "<p> In the early years of Spanish colonization in Marinduque, it was first mentioned in Miguel de Loarca's Relacion de las Yslas Filipinas (1582-1583) that the natives of the island are Pintados or tattooed people resembling those from the Camarines Provinces and the Visayan Islands.</p>";
+					echo "<p> In 1580, the first visita was established in the island of Marinduque and was called Montserrat de Marinduque in what is now the municipality of Boac; this visita was governed by Fray Alonzo Banol as minister. </br>In 1621, Spanish Jesuit missionaries brought a three-foot Marian image to Boac which will later be known as the Mahal na Ina ng Biglang Awa. Notorious for their sea raids, a group of Muslim pirates from the southern Philippines sieged Boac in the mid-17th century along the shores of Laylay and eventually into the Poblacion located along the Boac River.</br> In panic, the locals took refuge in the fortress church of Boac as able-bodied men defended the outer walls against the attacks. Many locals were killed and, by the third day of siege, the surviving locals began to run short on food and the capture of the fortress became imminent. As an act of faith, the survivors prayed fervently at the throne of Mary, asking her to deliver them from these enemies.</br>According to legends, a very strong storm with torrential rain, thunder, and lightning engulfed the area and at its very height, it was reported that the image of a beautiful lady with outstretched arms appeared standing on the top of the fortress wall. Terrorized, the pirates fled in confusion to their vintas and left the island. Since then, the image of Mary has been honoured and given the title Biglang Awa. To commemorate this miracle, the old image brought by the Jesuits in Boac was placed in a shrine erected on the portion of the wall where Mary is said to have appeared.</p>";
+					echo "<p> In 1942, Boac was occupied by Japanese troops. In 1945, the Battle of Marinduque began and the American-Philippine Commonwealth troops landed in Boac after the war was built of the general headquarters of the Philippine Commonwealth Army and Philippine Constabulary from 1945 to 1946 station in this municipality.The Provincial Government of Marinduque under Gov. Carmencita O. Reyes built a larger-than-life-size image of the Our Lady of Biglang Awa in cement and placed in the seashore in Balanacan Port to welcome travelers to the island.</br> On May 10, 2008, the Diocese of Boac celebrated the 13th anniversary and the golden anniversary of the canonical coronation of Mahal na Birhen ng Biglang-Awa (1958–2008). Cebu Archbishop-Cardinal Ricardo Vidal of Mogpog, Marinduque, officiated at the consecrated mass.</p>";		
+					?>
+				</div>
+				
+				<div class="footer">
+					<p>&copy; 2022 Marinduque State College<br> Lark Louie M. Rabi, Lupac,Boac, Marinduque- Philippines<br>.All rights reserved.</p>
+					
+					<a href="https://www.facebook.com/louie.rabi"><img src="images/fb.jpg"></a>
+					<a href="https://www.instagram.com/larkkkyyy/"><img src="images/ig.jpg"></a>
+				</div>
+		
+		</div>
+	</body>
+</html>
