5cedb70d1b168148a98dd05b6fcdadd97d1000f2
lark02 <louierabi2002@gmail.com>
2022-05-20 12:06:08 +0800

    tour added

---
+++ tour1.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Boac Tour</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <img src="images/boachead.png">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="tour.php"class="active">Tours</a></li>
+                    <li><a href="about-us.php">About us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+
+           <div class="places">
+                <div class="middle">
+                    <h2>Place to Visit</h2>
+                 <ul>
+                     <li><a href="tour1.php">Boac Cathedral</a></li>
+                     <li><a href="tour2.php">Casa Real</a></li>
+                     <li><a href="tour3.php">Boac National Museum</a></li>
+                     <li><a href="tour4.php">Biglang Awa Bridge</a></li>
+                     <li><a href="tour5.php">Laylay Port</a></li>
+                     <li><a href="tour6.php">Battle of Paye</a></li>
+                     <li><a href="tour7.php">Trivinios' House</a></li>
+                     <li><a href="tour8.php">Casa Narvas</a></li>
+                 </ul>
+                </div>
+            </div>
+            
+			
+			 <div class="Tour1">
+                   <div>
+                    <h2>Boac Cathedral</h2>
+                    <img src="images/tour1.jpg">
+                    <h4> source: <a href="https://en.wikipedia.org/wiki/Boac_Cathedral"> Boac Cathedral Image Link</a> </h4>
+                    <?php
+                    echo "<p> The Immaculate Conception Cathedral Parish (Filipino: Parokyang Katedral ng Kalinis-linisang Paglilihi), commonly referred to as Boac Cathedral, is a Roman Catholic church and cathedral in the town center of Boac, Marinduque, in the Mimaropa region of the Philippines.The seat of the Diocese of Boac, the cathedral today is surrounded by its likewise centuries-old defensive walls, and was declared a Philippine Important Cultural Property (ICP) in 2018 by the national museum of the country.</p>";
+                    ?>
+                    </div>
+            </div>
+            
+			
+        </div>
+     </div>
+    
+    </body>
+</html>
