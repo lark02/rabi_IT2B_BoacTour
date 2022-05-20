d67216e4e87de3e31e39d20f6b69a4316c9b54fd
lark02 <louierabi2002@gmail.com>
2022-05-20 12:50:45 +0800

    tour updated

---
+++ tour8.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Boac Tour</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <img src="images/boachead.png">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="tour.php"class="active">Tours</a></li>
+                    <li><a href="about-us.php">About us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+
+           <div class="places">
+                <div class="middle">
+                    <h2>Place to Visit</h2>
+                 <ul>
+                     <li><a href="tour1.php">Boac Cathedral</a></li>
+                     <li><a href="tour2.php">Casa Real</a></li>
+                     <li><a href="tour3.php">Boac National Museum</a></li>
+                     <li><a href="tour4.php">Biglang Awa Bridge</a></li>
+                     <li><a href="tour5.php">Laylay Port</a></li>
+                     <li><a href="tour6.php">Battle of Paye</a></li>
+                     <li><a href="tour7.php">Trivinios' House</a></li>
+                     <li><a href="tour8.php">Casa Narvas</a></li>
+                 </ul>
+                </div>
+            </div>
+            
+			
+			<div class="Tour1">
+                <div>
+                       <h2>Casa Narvas</h2>
+                       <img src="images/tour9.jpg">
+                       <h4> source: <a href="https://vymaps.com/PH/Casa-Real-373335/"> Casa Narvas Image Link </a> </h4>
+                       <?php
+                       echo  "<p>It was built in 1920, by Elias Narvas, a local coconut trader in those bygone years, and was called ‘bahay na bato’ also characterized by its massive red hardwood flooring, a balaustre and an azotea of Spanish and Chinese influence. It’s design included capiz-shelled sliding windows and stained glass walls. Narvas’ wife was named Vicenta Miciano Narvas and the house is said to have been built in memory of Bartolome and Hilda Narvas, Elias’ parents. During those years it became a hub for social and political gatherings being in the town center. Some dignitaries both foreign and local have dropped by this house to attend a ceremony or to be hosted for a while as house guests.Among these eminent personalities were Leonard Wood who was Governor-General of the Philippines  from 1921-1927, former Philippine Presidents Manuel L. Quezon, Jose P. Laurel, Manuel A. Roxas and Elpidio Quirino.</p>";
+                       ?>
+                </div>
+            </div>
+            
+            
+			
+        </div>
+     </div>
+    
+    </body>
+</html>
