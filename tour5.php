8d39e33d6c1267c1e906c3a16932afc0908d2735
lark02 <louierabi2002@gmail.com>
2022-05-20 12:29:00 +0800

    tour updated

---
+++ tour5.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Boac Tour</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <img src="images/boachead.png">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="tour.php"class="active">Tours</a></li>
+                    <li><a href="about-us.php">About us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+
+           <div class="places">
+                <div class="middle">
+                    <h2>Place to Visit</h2>
+                 <ul>
+                     <li><a href="tour1.php">Boac Cathedral</a></li>
+                     <li><a href="tour2.php">Casa Real</a></li>
+                     <li><a href="tour3.php">Boac National Museum</a></li>
+                     <li><a href="tour4.php">Biglang Awa Bridge</a></li>
+                     <li><a href="tour5.php">Laylay Port</a></li>
+                     <li><a href="tour6.php">Battle of Paye</a></li>
+                     <li><a href="tour7.php">Trivinios' House</a></li>
+                     <li><a href="tour8.php">Casa Narvas</a></li>
+                 </ul>
+                </div>
+            </div>
+            
+			
+			<div class="Tour1">
+                <div>
+                       <h2>Laylay Port</h2>
+                       <img src="images/tour6.jpg">
+                       <h4> source: <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fzh-cn.facebook.com%2Fgwapongmarinduque%2Fposts%2Fbeautiful-sunset-at-laylay-port-boacphotos-courtesy-bypatrick-dave-herrera-zolet%2F2803574053029187%2F&psig=AOvVaw2LY-0qaMQRzQaYa9myGZMg&ust=1653038562913000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJCNsNue6_cCFQAAAAAdAAAAABAJ">Laylay Port Image Link </a> </h4>
+                       <?php
+                        echo "<p>  Daungan ng Laylay, which, due to its strategic location, served as the landing port during the Spanish, American and Japanese periods of colonization. It was also the site where Governor General Leonard Wood landed on December 10, 1922, Governor General Roosevelt on April 10, 1932 and other national personages on various occasions.</p>";
+                        ?>
+                </div>
+            </div>
+            
+            
+			
+        </div>
+     </div>
+    
+    </body>
+</html>
