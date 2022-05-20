687e73b2507ad8fe45b746df2a95fcc820aff303
lark02 <louierabi2002@gmail.com>
2022-05-20 11:10:27 +0800

    tour nav added

---
+++ tour.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Boac Tour</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <img src="images/boachead.png">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="tour.php"class="active">Tours</a></li>
+                    <li><a href="about-us.php">About us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+           	
+
+           	<div class="places">
+            	<div class="middle">
+					<h2>Place to Visit</h2>
+                 <ul>
+                     <li><a href="tour1.php">Boac Cathedral</a></li>
+                     <li><a href="tour2.php">Casa Real</a></li>
+					 <li><a href="tour3.php">Boac National Museum</a></li>
+					 <li><a href="tour4.php">Biglang Awa Bridge</a></li>
+					 <li><a href="tour5.php">Laylay Port</a></li>
+					 <li><a href="tour6.php">Battle of Paye</a></li>
+					 <li><a href="tour7.php">Trivinios' House</a></li>
+					 <li><a href="tour8.php">Casa Narvas</a></li>
+                 </ul>
+				</div>
+			</div>
+			
+			
+        </div>
+     </div>
+    
+    </body>
+</html>
