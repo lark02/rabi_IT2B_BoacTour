d88a72b1c662575b1fb52ab9da40afc4cadd144c
lark02 <louierabi2002@gmail.com>
2022-05-20 12:11:08 +0800

    tour updated

---
+++ tour2.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Boac Tour</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <img src="images/boachead.png">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="tour.php"class="active">Tours</a></li>
+                    <li><a href="about-us.php">About us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+
+           <div class="places">
+                <div class="middle">
+                    <h2>Place to Visit</h2>
+                 <ul>
+                     <li><a href="tour1.php">Boac Cathedral</a></li>
+                     <li><a href="tour2.php">Casa Real</a></li>
+                     <li><a href="tour3.php">Boac National Museum</a></li>
+                     <li><a href="tour4.php">Biglang Awa Bridge</a></li>
+                     <li><a href="tour5.php">Laylay Port</a></li>
+                     <li><a href="tpur6.php">Battle of Paye</a></li>
+                     <li><a href="tour7.php">Trivinios' House</a></li>
+                     <li><a href="tour8.php">Casa Narvas</a></li>
+                 </ul>
+                </div>
+            </div>
+            
+			
+			<div class="Tour1">
+                <div>
+                       <h2>Casa Real</h2>
+                       <img src="images/tour2.jpg">
+                       <h4> source: <a href="https://vymaps.com/PH/Casa-Real-373335/"> Casa Real Image Link </a> </h4>
+                        
+                        <?php
+                        echo "<p>The old Casa Real used to be the official residence of the Gobernadorcillo and Gobernador, that, is, the Spanish Governor who made annual visits when Marinduque was under the 18th to the 19th century administration of Mindoro province. Town officials led by the Gobernadorcillo (Boac had its first Gobernadorcillo, town head, in 1735, in the person of Matias de Belen), would gather at the Port of Laylay to await his arrival, with people lining up the streets from Santol to the Casa Real for this day of celebration. Houses would display Spanish flags made of sinamay to greet some 50 horseback-riding uniformed men cuadrilleros, policemen, in gala, as paso-doble is played by a brass band.</p>";
+                        ?>
+                </div>
+            </div>
+            
+            
+			
+        </div>
+     </div>
+    
+    </body>
+</html>
