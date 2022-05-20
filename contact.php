9c0a5a7004bd2d7b1dd4f4a2858fa306ee0fac18
lark02 <louierabi2002@gmail.com>
2022-05-20 11:29:12 +0800

    contact nav added

---
+++ contact.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Boac Tour</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <img src="images/boachead.png">
+            </div>
+            <div class="nav">
+                <ul>
+					<li><a href="home.php">Home</a></li>
+                    <li><a href="tour.php">Tours</a></li>
+                    <li><a href="about-us.php">About us</a></li>
+                    <li><a href="contact.php"class="active">Contact</a></li>
+                </ul>
+			</div>
+			
+                 <div class="Contact">
+				<ul>
+                <a href="https://www.facebook.com/louie.rabi"><img src="images/fb.jpg"></a>
+                <a href="https://www.instagram.com/larkkkyyy/"><img src="images/ig.jpg"></a>
+				</ul>
+                </div>
+    
+    </body>
+</html>
