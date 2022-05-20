50b9b3d97dbed4e52b21faf979d1387595a218d3
lark02 <louierabi2002@gmail.com>
2022-05-20 12:21:28 +0800

    tour updated

---
+++ tour4.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Boac Tour</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <img src="images/boachead.png">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="tour.php"class="active">Tours</a></li>
+                    <li><a href="about-us.php">About us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+
+           <div class="places">
+                <div class="middle">
+                    <h2>Place to Visit</h2>
+                 <ul>
+                     <li><a href="tour1.php">Boac Cathedral</a></li>
+                     <li><a href="tour2.php">Casa Real</a></li>
+                     <li><a href="tour3.php">Boac National Museum</a></li>
+                     <li><a href="tour4.php">Biglang Awa Bridge</a></li>
+                     <li><a href="tour5.php">Laylay Port</a></li>
+                     <li><a href="tour6.php">Battle of Paye</a></li>
+                     <li><a href="tour7.php">Trivinios' House</a></li>
+                     <li><a href="tour8.php">Casa Narvas</a></li>
+                 </ul>
+                </div>
+            </div>
+            
+			
+			<div class="Tour1">
+                <div>
+                       <h2>Biglang Awa Bridge</h2>
+                       <img src="images/tour5.jpg">
+                       <h4> source: <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DLfATGew78JY&psig=AOvVaw1F9AvCKkuH1PxSigSsqvAr&ust=1653037989246000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOCJscec6_cCFQAAAAAdAAAAABAD">Biglang Awa Image Link </a> </h4>
+                       <?php
+                       echo "<p>According to legends, a very strong storm with torrential rain, thunder, and lightning engulfed the area and at its very height, it was reported that the image of a beautiful lady with outstretched arms appeared standing on the top of the fortress wall. Terrorized, the pirates fled in confusion to their vintas and left the island. Since then, the image of Mary has been honoured and given the title Biglang Awa. To commemorate this miracle, the old image brought by the Jesuits in Boac was placed in a shrine erected on the portion of the wall where Mary is said to have appeared.</p>";
+                       ?>
+                </div>
+            </div>
+            
+            
+			
+        </div>
+     </div>
+    
+    </body>
+</html>
