cdb75f787152513a8e1b3e9077b877c7c61de245
lark02 <louierabi2002@gmail.com>
2022-05-20 11:22:34 +0800

    about us nav added

---
+++ about-us.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Boac Tour</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <img src="images/boachead.png">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="tour.php">Tours</a></li>
+                    <li><a href="about-us.php"class="active">About us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+			</div>
+                
+			<div class="About-us">
+				<div>
+					   <h2>Boac Tour</h2>
+					   <h4>"Provides information and knowledge about the tourists spots most-likey the ancestral buildings that have contribute to history of Boakeños."</br>-Lark Louie Rabi-BSI/T-2B</h4>
+					   
+				</div>
+			</div>
+    </body>
+</html>
