bd5ca6485a440321bc3fc75f583e403857148961
lark02 <louierabi2002@gmail.com>
2022-05-20 11:45:59 +0800

    where is boac added

---
+++ whereisboac.php
@@ -0,0 +1,65 @@
+
+
+
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>BOAC TOUR</title>
+		<link rel="stylesheet" href="css/style.css">
+		
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="header">
+				<img src="images/boachead.png">
+			</div>
+		<div class="nav">
+				<ul>
+					<li><a href="home.php"  class="active">Home</a></li>
+					<li><a href="tour.php">Tours</a></li>
+					<li><a href="about-us.php">About us</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+			</div>
+			<div class="banner">
+				<img src="images/banner.png">
+			</div>
+			
+			<div class="main">
+				<div class="left-side-bar">
+					<h2>Menu</h2>
+                 <ul>
+                     <li><a href="whatisboac.php">What is Boac?</a></li>
+                     <li><a href="whereisboac.php" >Where is Boac?</a></li>
+					 <li><a href="history.php">History</a></li>
+                 </ul>
+				</div>
+				
+				<div class="right-side">
+					<img src="images/map.png">
+				</div>
+				
+			
+				<div class="content">
+					<h1>Home</h1>
+					
+					<h2 id="Where is Boac?"> Where is Boac?</h2>
+					<?php
+					echo "<p> Situated in the western coast of Marinduque Island, the municipality is bordered in the north by the municipality of Mogpog, in the west by the Tayabas Bay and Sibuyan Sea, in the east by the municipality of Torrijos, and in the south by the municipalities of Gasan and Buenavista.</p>";
+					?>
+					
+				</div>
+				
+				<div class="footer">
+					<p>&copy; 2022 Marinduque State College<br> Lark Louie M. Rabi, Lupac,Boac, Marinduque- Philippines<br>.All rights reserved.</p>
+					
+					<a href="https://www.facebook.com/louie.rabi"><img src="images/fb.jpg"></a>
+					<a href="https://www.instagram.com/larkkkyyy/"><img src="images/ig.jpg"></a>
+				</div>
+		
+		</div>
+	</body>
+</html>
