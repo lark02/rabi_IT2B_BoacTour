0634b8d7d55b5caa1d0e92ea29a4174e759c768a
lark02 <louierabi2002@gmail.com>
2022-05-20 12:36:32 +0800

    tour updated

---
+++ tour6.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Boac Tour</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <img src="images/boachead.png">
+            </div>
+            <div class="nav">
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="tour.php"class="active">Tours</a></li>
+                    <li><a href="about-us.php">About us</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </div>
+
+           <div class="places">
+                <div class="middle">
+                    <h2>Place to Visit</h2>
+                 <ul>
+                     <li><a href="tour1.php">Boac Cathedral</a></li>
+                     <li><a href="tour2.php">Casa Real</a></li>
+                     <li><a href="tour3.php">Boac National Museum</a></li>
+                     <li><a href="tour4.php">Biglang Awa Bridge</a></li>
+                     <li><a href="tour5.php">Laylay Port</a></li>
+                     <li><a href="tour6.php">Battle of Paye</a></li>
+                     <li><a href="tour7.php">Trivinios' House</a></li>
+                     <li><a href="tour8.php">Casa Narvas</a></li>
+                 </ul>
+                </div>
+            </div>
+            
+			
+			<div class="Tour1">
+                <div>
+                       <h2>Battle of Paye</h2>
+                       <img src="images/tour8.jpg">
+                       <h4> source: <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.com.ph%2FAttraction_Review-g1026955-d3654232-Reviews-Battle_of_Paye_Marker-Marinduque_Island_Mimaropa.html&psig=AOvVaw09ALzBETJbeF0cFE-rgihL&ust=1653039169763000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJCM2f-g6_cCFQAAAAAdAAAAABAD"> Battle of Paye Image Link </a> </h4>
+                       <?php
+                       echo  "<p>The Battle of Paye, also known as the Battle of San Mateo, was a battle during the Philippine–American War between the United States and the Philippines. It was fought on December 19, 1899, near San Mateo in what was then Manila province (now Rizal) between the forces of General Henry Ware Lawton, and General Licerio Gerónimo's Morong Command battalion and the Tiradores de la Muerte. Lawton was killed in the battle, making him the highest-ranking American commander to die in the Philippine conflict.On December 18, Lawton and his men were en route to San Mateo along the Marikina River in a punitive expedition against Brig. Gen. Pio del Pilar's 1,000 force, which threatened the Marikina waterworks and the Manila wagon road to the north.Lawton's force included Col. James R. Lockett's squadron of the 11th Volunteer Cavalry and Lt. Col. H.H. Sargent's 29th Battalion. A monsoon flooded the river and muddied the trail</p>";
+                       ?>
+                </div>
+            </div>
+            
+            
+			
+        </div>
+     </div>
+    
+    </body>
+</html>
